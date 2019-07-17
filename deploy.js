3880ecb21c9096d9ab6ab9b7d1580497e691c097
RDUlew <lillew9@gmail.com>
2019-07-17 10:40:30 -0400

    pem to ppk

--- deploy.js
+++ deploy.js
@@ -87,7 +87,7 @@ function sshConnect() {
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
       host: '54.89.237.59',
       username: 'ubuntu',
-      privateKey: 'Final-key.pem'
+      privateKey: 'Final-key.ppk'
     })
     .then(function() {
       console.log('SSH Connection established.');
